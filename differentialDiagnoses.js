a0b31e4d67ffd61483d8c65c10f2904275c14ed6
Rachel Draelos <rachel.draelos@gmail.com>
2025-11-24 15:12:23 -0500

    ddx

---
+++ differentialDiagnoses.js
@@ -0,0 +1,21 @@
+export const respiratoryAcidosisDiff =
+    'Pulmonary disease: COPD, asthma, acute respiratory distress syndrome (ARDS)' +
+    ',pneumonia, bronchitis, airway obstruction/foreign body aspiration, cardiogenic pulmonary edema,' +
+    ' acute lung injury, pulmonary fibrosis, pleural effusion. \n' +
+    'Decreased chest wall or diaphragm movement: obesity, diaphragmatic dysfunction or paralysis, neuromuscular disorders, Guillain-Barre, myasthenia gravis,' +
+    ' demyelinating disorders, tetanus, muscle relaxants, ' +
+    'organophosphates, botulism, acute chest trauma, chest wall deformity, tension pneumothorax' +
+    '\nDecreased respiratory drive: stroke, CNS tumor, encephalitis, CNS hemorrhage, medication use' +
+    ' (opioids, alcohol, sedatives, hypnotics, anxiolytics, anesthetics) \n';
+
+export const respiratoryAlkalosisDiff =
+    'Acute respiratory distress syndrome (ARDS), asthma, atrial fibrillation, atrial flutter, head trauma, heatstroke, hyperthyroidism, meningitis, myocardial infarction, panic disorder, pneumonia, pneumothorax, pregnancy, pulmonary edema, pulmonary embolism, idiopathic pulmonary fibrosis, sepsis, salicylate toxicity, theophylline toxicity \n';
+
+export const HAGMADiff =
+    'GOLDMARK: glycols (ethylene glycol, propylene glycol), oxoproline (from excess acetaminophen), L-Lactate (lactic acidosis), D-Lactate (from gut bacteria), methanol (and other alcohols), aspirin/salicylate toxicity, renal failure (uremia), ketones (diabetic, alcoholic, starvation) \n';
+
+export const NAGMADiff =
+    'With any K: administration of IVF containing Cl- \nWith high/normal K: administration of HCl (TPN, NH4Cl), hyperkalemic distal RTA (hyporeninemic hypoaldosteronism, tubular resistance to aldosterone, aldosterone deficiency), chronic renal failure, Gordon’s syndrome, decreased distal Na delivery, drugs (triamterene, amiloride, pentamidine, NSAIDs, CEIs, ARBs, trimethoprim, spironolactone, heparin)\nWith low K: diarrhea, intestinal fistulae, proximal RTA, distal RTA, ureteroileostomy, ureterosigmoidostomy, toluene intoxication, ketoacidosis, D-lactic acidosis \n';
+
+export const metabolicAlkalosisDiff =
+    'Hydrogen ion loss via vomiting, gastric secretion losses e.g. via NG suctioning, shift of hydrogen ions intracellularly from hypokalemia, bicarbonate administration, citrate in transfused blood, metabolism of ketoanions to produce bicarb, contraction alkalosis (loop or thiazide diuretic administration, diarrhea, or any excessive loss of volume), primary or secondary hyperaldosteronism, renal artery stenosis, Cushing syndrome, Liddle syndrome, Bartter syndrome, Gitelman syndrome, congenital chloridorrhea \n';
